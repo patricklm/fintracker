f200b496acd341b4c969fc094146141654bdf0c0
Patrick Mashaba <patrickmashaba@gmail.com>
2024-06-29 17:07:14 +0200

    Added transaction component

---
+++ app/components/transaction.vue
@@ -0,0 +1,43 @@
+<script setup>
+const { currency } = useCurrency(3000);
+const items = [
+  [
+    {
+      label: 'Edit',
+      icon: 'i-heroicons-pencil-square-20-solid',
+      click: () => console.log('Edit'),
+    },
+    {
+      label: 'Delete',
+      icon: 'i-heroicons-trash-20-solid',
+      click: () => console.log('Delete'),
+    },
+  ],
+];
+</script>
+
+<template>
+  <div
+    class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100"
+  >
+    <div class="flex items-center justify-between">
+      <div class="flex items-center space-x-1">
+        <UIcon name="i-heroicons-arrow-up-right" class="text-gray-600" />
+        <div>Salary</div>
+      </div>
+      <UBadge>Category</UBadge>
+    </div>
+    <div class="flex items-center justify-end">
+      <div>{{ currency }}</div>
+      <div>
+        <UDropdown :items="items" :popper="{ placement: 'bottom-end' }">
+          <UButton
+            color="white"
+            variant="ghost"
+            trailing-icon="i-heroicons-ellipsis-horizontal"
+          />
+        </UDropdown>
+      </div>
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,40 +4,50 @@ const selectedView = ref(transactionViewOptions[1]);
 </script>
 
 <template>
-  <section class="flex items-center justify-between mb-10">
-    <h1 class="text-4xl font-extrabold">Summary</h1>
-    <div>
-      <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
-    </div>
-  </section>
-  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-16 mb-10">
-    <Trend
-      color="green"
-      title="Income"
-      :amount="42000"
-      :last-amount="35500"
-      :loading="false"
-    />
-    <Trend
-      color="red"
-      title="Expense"
-      :amount="13500"
-      :last-amount="15500"
-      :loading="false"
-    />
-    <Trend
-      color="green"
-      title="Investment"
-      :amount="46000"
-      :last-amount="25000"
-      :loading="false"
-    />
-    <Trend
-      color="red"
-      title="Saving"
-      :amount="15000"
-      :last-amount="10000"
-      :loading="false"
-    />
-  </section>
+  <div>
+    <section class="flex items-center justify-between mb-10">
+      <h1 class="text-4xl font-extrabold">Summary</h1>
+      <div>
+        <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
+      </div>
+    </section>
+    <section
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-16 mb-10"
+    >
+      <Trend
+        color="green"
+        title="Income"
+        :amount="42000"
+        :last-amount="35500"
+        :loading="false"
+      />
+      <Trend
+        color="red"
+        title="Expense"
+        :amount="13500"
+        :last-amount="15500"
+        :loading="false"
+      />
+      <Trend
+        color="green"
+        title="Investment"
+        :amount="46000"
+        :last-amount="25000"
+        :loading="false"
+      />
+      <Trend
+        color="red"
+        title="Saving"
+        :amount="15000"
+        :last-amount="10000"
+        :loading="false"
+      />
+    </section>
+    <section>
+      <Transaction />
+      <Transaction />
+      <Transaction />
+      <Transaction />
+    </section>
+  </div>
 </template>
